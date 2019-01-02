98a2dc85726114c0a1dcf975b7f39021002b4952
benrob0329 <ben@innovationplex.com>
2019-01-02 17:56:46 -0500

    Refactoring, enable tin by default, remove ore generation (#14)
    Remove Ore Generation
    Disable Crafting Currency By Default
    Make All Currency Smelted From Ore
    Enable Tin Smelting By Default

--- minecoins.lua
+++ minecoins.lua
@@ -1,34 +1,4 @@
 -- Node definitions
-minetest.register_node("bitchange:minecoin_in_ground", {
-	description = "you hacker!",
-	tiles = { "default_stone.png^bitchange_minecoin_in_ground.png" },
-	is_ground_content = true,
-	groups = {cracky=2, not_in_creative_inventory=1},
-	sounds = default.node_sound_stone_defaults(),
-	drop = {
-		max_items = 2,
-		items = {
-			{items = {"bitchange:minecoin"}, rarity = 2 },
-			{items = {"bitchange:minecoin 3"} }
-		}
-	},
-})
-
-minetest.register_node("bitchange:mineninth_in_ground", {
-	description = "you hacker!",
-	tiles = { "default_stone.png^bitchange_mineninth_in_ground.png" },
-	is_ground_content = true,
-	groups = {cracky=3, not_in_creative_inventory=1},
-	sounds = default.node_sound_stone_defaults(),
-	drop = {
-		max_items = 3,
-		items = {
-			{items = {"bitchange:mineninth 4"}, rarity = 3 },
-			{items = {"bitchange:mineninth 6"} }
-		}
-	},
-})
-
 minetest.register_node("bitchange:minecoinblock", {
 	description = "MineCoin Block",
 	tiles = { "bitchange_minecoinblock.png" },
@@ -49,40 +19,40 @@ minetest.register_craftitem("bitchange:mineninth", {
 	stack_max = 30000,
 })
 
-minetest.register_alias("bitchange:coinbase", "bitchange:mineninth")
-
 -- Crafting
-minetest.register_craft({
-	output = "bitchange:minecoinblock",
-	recipe = {
-		{"bitchange:minecoin", "bitchange:minecoin", "bitchange:minecoin"},
-		{"bitchange:minecoin", "bitchange:minecoin", "bitchange:minecoin"},
-		{"bitchange:minecoin", "bitchange:minecoin", "bitchange:minecoin"},
-	}
-})
+if bitchange.craft_convert_currency then
+	minetest.register_craft({
+		output = "bitchange:minecoinblock",
+		recipe = {
+			{"bitchange:minecoin", "bitchange:minecoin", "bitchange:minecoin"},
+			{"bitchange:minecoin", "bitchange:minecoin", "bitchange:minecoin"},
+			{"bitchange:minecoin", "bitchange:minecoin", "bitchange:minecoin"},
+		}
+	})
 
-minetest.register_craft({
-	output = "bitchange:minecoin 9",
-	recipe = {
-		{"bitchange:minecoinblock"},
-	}
-})
+	minetest.register_craft({
+		output = "bitchange:minecoin 9",
+		recipe = {
+			{"bitchange:minecoinblock"},
+		}
+	})
 
-minetest.register_craft({
-	output = "bitchange:minecoin",
-	recipe = {
-		{"bitchange:mineninth", "bitchange:mineninth", "bitchange:mineninth"},
-		{"bitchange:mineninth", "bitchange:mineninth", "bitchange:mineninth"},
-		{"bitchange:mineninth", "bitchange:mineninth", "bitchange:mineninth"},
-	}
-})
+	minetest.register_craft({
+		output = "bitchange:minecoin",
+		recipe = {
+			{"bitchange:mineninth", "bitchange:mineninth", "bitchange:mineninth"},
+			{"bitchange:mineninth", "bitchange:mineninth", "bitchange:mineninth"},
+			{"bitchange:mineninth", "bitchange:mineninth", "bitchange:mineninth"},
+		}
+	})
 
-minetest.register_craft({
-	output = "bitchange:mineninth 9",
-	recipe = {
-		{"bitchange:minecoin"},
-	}
-})
+	minetest.register_craft({
+		output = "bitchange:mineninth 9",
+		recipe = {
+			{"bitchange:minecoin"},
+		}
+	})
+end
 
 -- Cooking
 minetest.register_craft({
@@ -97,38 +67,13 @@ minetest.register_craft({
 	output = "default:gold_ingot 4",
 })
 
--- Generation
+-- Legacy
 if bitchange.enable_generation then
-minetest.register_ore({
-	ore_type       = "scatter",
-	ore            = "bitchange:minecoin_in_ground",
-	wherein        = "default:stone",
-	clust_scarcity = 15*15*15,
-	clust_num_ores = 3,
-	clust_size     = 7,
-	y_max          = -512,
-	y_min          = -18000,
-})
-
-minetest.register_ore({
-	ore_type       = "scatter",
-	ore            = "bitchange:mineninth_in_ground",
-	wherein        = "default:stone",
-	clust_scarcity = 12*12*12,
-	clust_num_ores = 5,
-	clust_size     = 8,
-	y_max          = -256,
-	y_min          = -511,
-})
-
-minetest.register_ore({
-	ore_type       = "scatter",
-	ore            = "bitchange:mineninth_in_ground",
-	wherein        = "default:stone",
-	clust_scarcity = 13*13*13,
-	clust_num_ores = 3,
-	clust_size     = 7,
-	y_max          = 28000,
-	y_min          = -255,
-})
+	minetest.log("warning", "[bitchange] Ores will not be generated any more. Remove the setting " ..
+		"'enable_generation' from your bitchange configuration to suppress this warning.")
 end
+
+minetest.register_alias("bitchange:minecoin_in_ground", "default:stone_with_gold")
+minetest.register_alias("bitchange:mineninth_in_ground", "default:stone_with_tin")
+
+minetest.register_alias("bitchange:coinbase", "bitchange:mineninth")
--- moreores.lua
+++ moreores.lua
@@ -2,24 +2,9 @@
 
 if bitchange.use_technic_zinc and minetest.get_modpath("technic_worldgen") then
 	minetest.register_craft({
+		type   = "cooking",
 		output = "bitchange:mineninth 8",
-		recipe = {
-			{"technic:zinc_block", "default:pick_diamond"},
-			{"technic:zinc_block", ""}
-		},
-		replacements = { {"default:pick_diamond", "default:pick_diamond"} }
-	})
-end
-
-if bitchange.use_quartz and minetest.get_modpath("quartz") then
-	minetest.register_craft({
-		output = "bitchange:mineninth",
-		recipe = {
-			{"quartz:quartz_crystal", "default:pick_diamond"},
-			{"quartz:quartz_crystal", "quartz:quartz_crystal"},
-			{"quartz:quartz_crystal", "quartz:quartz_crystal"}
-		},
-		replacements = { {"default:pick_diamond", "default:pick_diamond"} }
+		recipe = "technic:zinc_block",
 	})
 end
 
@@ -30,11 +15,8 @@ end
 
 if bitchange.use_default_tin then
 	minetest.register_craft({
+		type   = "cooking",
 		output = "bitchange:mineninth 18",
-		recipe = {
-			{"default:tinblock", "default:pick_diamond"},
-			{"default:tinblock", ""}
-		},
-		replacements = { {"default:pick_diamond", "default:pick_diamond"} }
+		recipe = "default:tinblock"
 	})
 end
